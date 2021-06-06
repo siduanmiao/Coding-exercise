e7c965cbfab4fd39db79a8327987fb433eab2c56
siduanmiao <85001487+siduanmiao@users.noreply.github.com>
2021-06-06 14:28:22 +0800

    Create Dijkstra.h

---
+++ Dijkstra.h
@@ -0,0 +1,19 @@
+//Dijkstra.h
+#ifndef TEST_H_INCLUDED
+#define TEST_H_INCLUDED
+
+//加上下面这段话
+
+
+#ifdef __cplusplus
+extern "C" {
+#endif // __cplusplus
+
+#include "common.h"
+
+#ifdef __cplusplus
+}
+#endif // __cplusplus
+
+
+#endif // TEST_H_INCLUDED
