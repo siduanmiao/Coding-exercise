19c1a6b8860e762bcb6980f27a9a2fd7222db801
siduanmiao <85001487+siduanmiao@users.noreply.github.com>
2021-06-06 14:22:39 +0800

    Create usage.c

---
+++ usage.c
@@ -0,0 +1,12 @@
+//usage.c
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <malloc.h>
+#include "common.h"
+void usage (){
+    
+    printf("Your protien is not contained in our database! Please check your input agian.\n");
+    exit(1);
+    
+}
